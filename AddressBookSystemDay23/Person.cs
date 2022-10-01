f8c544b0e66309da13ef7062ad2c442d9b5dc442
LokeshKiran <lokeshkskiran@gmail.com>
2022-10-01 20:36:22 +0530

    [Lokesh]Added:Searching dictionary using name in the dictionary

--- AddressBookSystemDay23/Person.cs
+++ AddressBookSystemDay23/Person.cs
@@ -8,5 +8,20 @@ namespace AddressBookSystemDay23
 {
     internal class Person
     {
+        public string firstName { get; set; }
+
+        public string lastName { get; set; }
+
+        public string address { get; set; }
+
+        public string city { get; set; }
+
+        public string state { get; set; }
+
+        public int zipCode { get; set; }
+
+        public string phoneNumber { get; set; }
+
+        public string email { get; set; }
     }
 }
