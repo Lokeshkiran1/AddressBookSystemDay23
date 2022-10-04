86de004b40fb51ad4794e68081bd87d3d63fb1e5
LokeshKiran <lokeshkskiran@gmail.com>
2022-10-04 21:27:48 +0530

    [Lokesh]Added:Ability to read/write the address book with persons contact as csv file

---
+++ CSVHandler.cs
@@ -0,0 +1,46 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Formats.Asn1;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookManagemenrtSystemDay28
+{
+    public class CSVHandler
+    {
+        string importFilePath = @"C:\Users\HP\Desktop\csharp\AddressBookManagemenrtSystemDay28\AddressBook.txt";
+
+        public void WriteToCsv(Dictionary<string, AddressBookBuilder> addressbookDictionary)
+        {
+            using (StreamWriter writer = new StreamWriter(importFilePath))
+            {
+                using (CsvWriter csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+                {
+                    foreach (AddressBookBuilder item in addressbookDictionary.Values)
+                    {
+                        List<Contacts> contactRecord = item.addressBook.Values.ToList();
+                        csv.WriteRecords(contactRecord);
+                    }
+                }
+            }
+        }
+        public void ReadFromCSV()
+        {
+            using (StreamReader reader = new StreamReader(importFilePath))
+            {
+                using (CsvReader csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+                {
+                    Console.WriteLine("Below are Contents of CSV File");
+                    List<Contacts> contactRecord = csv.GetRecords<Contacts>().ToList();
+                    foreach (Contacts contact in contactRecord)
+                    {
+                        Console.WriteLine(contact.ToString());
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ ReadWriteFileIO.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookManagemenrtSystemDay28
+{
+    internal class ReadWriteFileIO
+    {
+        static string file = @"C:\Users\HP\Desktop\csharp\AddressBookManagemenrtSystemDay28\AddressBook.txt";
+
+        public void WriteToFile(Dictionary<string, AddressBookBuilder> addressBookDictionary)
+        {
+            StreamWriter writer = new StreamWriter(file, true);
+            foreach (AddressBookBuilder item in addressBookDictionary.Values)
+            {
+                foreach (Contacts contact in item.addressBook.Values)
+                {
+                    writer.WriteLine(contact.ToString());
+                }
+            }
+            Console.WriteLine("\nSuccessfully added to Text file.");
+            writer.Close();
+        }
+        public void ReadFromFile()
+        {
+            Console.WriteLine(File.ReadAllText(file));
+        }
+    }
+}
